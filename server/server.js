4d2366bbcf69bee470d999b64cabaff79db47330
Richard Otis, Jr <richardotisjr@gmail.com>
2022-06-25 15:34:04 -0500

    Server Update

--- server/server.js
+++ server/server.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+const blogRoutes = require("./routes/blogRoutes");
+// const mongoose = require("mongoose");
+const connectDatabase = require("./databases/connectDatabase");
+
+// Connect Database
+connectDatabase();
 
 app.use(cors());
+app.use(express.json());
+// app.use(express.urlencoded());
+
+// Routes
+app.use("/api/blog", blogRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
